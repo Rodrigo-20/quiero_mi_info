2af8e535d0614cc0260e8af3ed8544fd942b4ea2
Rodrigo <ronico041012@gmail.com>
2022-03-10 18:21:02 -0300

    11 days left

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const changeSize = (e) => {
-    console.log(e.target.innerWidth)
+    console.log(clickedItem.innerWidth)
 
 }
 const menu = document.querySelector('.menu');
 const icon = document.querySelector('.burguer-icon')
 const nav = document.querySelector('nav');
 const wraper = document.querySelector('.top');
+const listItems = document.querySelectorAll('.top li');
+const list = document.querySelector('ul');
 
 window.addEventListener('resize', changeSize);
 
@@ -14,3 +16,31 @@ icon.addEventListener('click', () => {
     nav.classList.toggle('slideOut');
     wraper.classList.toggle("active");
 })
+
+list.addEventListener('click', (e) => {
+
+    clickedItem = e.target
+    if (clickedItem.tagName === 'LI') {
+        listItems.forEach(item => {
+            item.classList.remove('active');
+
+        })
+        clickedItem.classList.add('active');
+        clickedItem.querySelector('a').click();
+    }
+    else if (clickedItem.parentNode.tagName === 'LI') {
+        listItems.forEach(item => {
+            item.classList.remove('active');
+        })
+        clickedItem.parentNode.classList.add('active')
+    }
+
+
+})
+
+// listItems.forEach(item => {
+//     item.addEventListener('click', () => {
+//         item.classList.add('active');
+//     })
+
+// });
