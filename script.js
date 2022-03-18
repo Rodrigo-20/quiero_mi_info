2b9f39ea3cd74296e21e42afbbf7c244a2b1a8bd
Rodrigo <ronico041012@gmail.com>
2022-03-18 18:25:09 -0300

    nav terminado

--- script.js
+++ script.js
@@ -21,21 +21,30 @@ list.addEventListener('click', (e) => {
 
     clickedItem = e.target
     if (clickedItem.tagName === 'LI') {
-        listItems.forEach(item => {
-            item.classList.remove('active');
-
-        })
-        clickedItem.classList.add('active');
         clickedItem.querySelector('a').click();
     }
-    else if (clickedItem.parentNode.tagName === 'LI') {
-        listItems.forEach(item => {
-            item.classList.remove('active');
-        })
-        clickedItem.parentNode.classList.add('active')
-    }
 
+})
+//controla el estilo del nav dependiendo en que parte de la pagina me encuentre 
+document.addEventListener('scroll', (e) => {
+    listItems.forEach(item => {
+        let ref = item.querySelector('a').getAttribute('href');
+        let rect = document.querySelector(`${ref}`).getBoundingClientRect();
+        let top = rect.top;
+        let bottom = rect.bottom;
+        console.log(e)
+        console.log('bottom : ', top);
+        const height = window.innerHeight;
+        const topLimit = height * 0.2;
+        const bottomLimit = height * 0.8
+        if ((top <= topLimit && top >= 0) || (bottom >= bottomLimit && bottom < height)) {
+            listItems.forEach(item => {
+                item.classList.remove('active')
+            });
+            item.classList.add('active');
+        }
 
+    })
 })
 
 // listItems.forEach(item => {
